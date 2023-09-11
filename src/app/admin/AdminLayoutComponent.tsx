5ad85ea1dd73cb01df473b02fab343b189958ca3
Olufemi Olaosebikan <olufemiolaosebikan.t@gmail.com>
2023-09-11 15:24:07 -0700

    first
    added the Purchase page navigation

--- src/app/admin/AdminLayoutComponent.tsx
+++ src/app/admin/AdminLayoutComponent.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import InventoryModuleLayout from "./modules/inventory-module/InventoryModuleLayout";
 import SelectAModule from "./SelectAModule";
+import PurchaseModuleLayout from "./modules/purchase-module/PurchaseModuleLayout";
 
 const AdminLayoutComponent = () => {
     return (
@@ -8,6 +9,7 @@ const AdminLayoutComponent = () => {
             <Route path="/" element={<SelectAModule />} />
             <Route path="/modules" element={<SelectAModule />} />
             <Route path="modules/inventory/*" element={<InventoryModuleLayout />} />
+            <Route path="modules/purchases/*" element={<PurchaseModuleLayout />} />
         </Routes>
     );
 };
--- src/app/admin/modules/inventory-module/inventory-adjustments/InventoryAdjustmentsComponent.tsx
+++ src/app/admin/modules/inventory-module/inventory-adjustments/InventoryAdjustmentsComponent.tsx
@@ -5,3 +5,4 @@ const InventoryAdjustmentsComponent = () => {
 };
 
 export default InventoryAdjustmentsComponent;
+
