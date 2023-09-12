003d6c6099fc41cdaeec6aef58ac1e45bb391fc2
Olufemi Olaosebikan <olufemiolaosebikan.t@gmail.com>
2023-09-12 13:30:07 -0700

    update

--- src/app/admin/modules/sales-module/customerssalesinfo/CustomerSalesComponent.tsx
+++ src/app/admin/modules/sales-module/customerssalesinfo/CustomerSalesComponent.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Button, Flex, Icon, SimpleGrid } from "@chakra-ui/react";
+import CustomerSalesInfoTableComponent from "./CustomerSalesInfoTableComponent";
+import axios from "axios";
+import { apiBaseUrl } from "environment";
+import { MdAdd } from "react-icons/md";
 
 const CustomerSalesComponent = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [tableData, setTableData] = useState(null);
 
-export default CustomerSalesComponent
+    useEffect(() => {
+        // Make an Axios GET request to the API endpoint
+        axios
+            .get(apiBaseUrl + "api/Inventory/GetAllItems")
+            .then((response) => {
+                // Assuming the API response is in the expected format
+                if (response.data && response.data.data) {
+                    setTableData(response.data.data.items);
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <>
+            <Flex
+                pt={{ base: "130px", md: "80px", xl: "80px" }}
+                my="0px"
+                h="fit-content"
+                align={{ base: "center", xl: "center" }}
+                justify={{
+                    base: "flex-end",
+                    xl: "flex-end",
+                }}
+                gap="20px"
+            >
+                <Button leftIcon={<Icon as={MdAdd} width="20px" height="20px" color="inherit" />} variant="brand">
+                    Add New Customer
+                </Button>
+            </Flex>
+            <Box pt={{ base: "16px", md: "16px", xl: "16px" }}>
+                <SimpleGrid columns={{ base: 1, md: 1, xl: 1 }} gap="20px" mb="20px">
+                    {tableData && <CustomerSalesInfoTableComponent tableData={tableData} />}
+                </SimpleGrid>
+            </Box>
+        </>
+    );
+};
+
+export default CustomerSalesComponent;
---
+++ src/app/admin/modules/sales-module/customerssalesinfo/CustomerSalesInfoTableComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomerSalesInfoTableComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CustomerSalesInfoTableComponent
