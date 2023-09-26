c0b0500efa8be7947326c8fb4fb2da1c07bd8ab7
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2023-09-26 17:34:55 +0100

    Update environment.ts

--- src/environment.ts
+++ src/environment.ts
@@ -1,2 +1,3 @@
 //export const apiBaseUrl = "http://localhost:5192/api/";
-export const apiBaseUrl = "https://abiolasoft-finance-ui-az.azurewebsites.net/api/";
+//export const apiBaseUrl = "https://abiolasoft-finance-ui-az.azurewebsites.net/api/";
+export const apiBaseUrl = "http://api-dev.eba-umjxxmcj.eu-west-2.elasticbeanstalk.com/api/";
