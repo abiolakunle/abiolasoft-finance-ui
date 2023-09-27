4f3e57fcf054da1d996587a078e4ced1b4b9b593
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2023-09-27 01:30:44 +0100

    configure error toast

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,8 @@
 // api/axiosWrapper.js
 import axios from "axios";
 import { apiBaseUrl } from "environment";
-import { hideProgress, showProgress } from "state/slices/progressSlice";
+
+import { showProgress } from "state/slices/progressSlice";
 import { store } from "state/store";
 
 const axiosRequest = axios.create({
@@ -9,22 +10,8 @@ const axiosRequest = axios.create({
 });
 
 axiosRequest.interceptors.request.use((config) => {
-    // Dispatch action to show loader
     store.dispatch(showProgress());
     return config;
 });
-
-axiosRequest.interceptors.response.use(
-    (response) => {
-        // Dispatch action to hide loader on successful response
-        store.dispatch(hideProgress());
-        return response;
-    },
-    (error) => {
-        // Dispatch action to hide loader on error
-        store.dispatch(hideProgress());
-        return Promise.reject(error);
-    }
-);
 
 export default axiosRequest;
