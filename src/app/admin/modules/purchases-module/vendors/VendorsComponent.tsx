cc2f2271e8819f89b13dd402a2e7c89a8c2bf783
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2023-09-27 22:14:57 +0100

    create vendor table

--- src/app/admin/modules/purchases-module/vendors/VendorsComponent.tsx
+++ src/app/admin/modules/purchases-module/vendors/VendorsComponent.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Box, Button, Flex, Icon, SimpleGrid } from "@chakra-ui/react";
+import VendorsTableComponent from "./VendorsTableComponent";
+import { MdAdd } from "react-icons/md";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import axiosRequest from "utils/api";
 
 const VendorsComponent = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const [tableData, setTableData] = useState(null);
 
-export default VendorsComponent
+    useEffect(() => {
+        axiosRequest
+            .get("Purchases/GetAllVendors?PageIndex=1&PageSize=50")
+            .then((response) => {
+                if (response.data && response.data.data) {
+                    setTableData(response.data.data.items);
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, []);
+
+    return (
+        <>
+            <Flex
+                pt={{ base: "130px", md: "80px", xl: "80px" }}
+                my="0px"
+                h="fit-content"
+                align={{ base: "center", xl: "center" }}
+                justify={{
+                    base: "flex-end",
+                    xl: "flex-end",
+                }}
+                gap="20px"
+            >
+                <ChakraLink as={ReactRouterLink} to={`/admin/modules/purchases/vendors/new`}>
+                    <Button leftIcon={<Icon as={MdAdd} width="20px" height="20px" color="inherit" />} variant="brand">
+                        New
+                    </Button>
+                </ChakraLink>
+            </Flex>
+            <Box pt={{ base: "16px", md: "16px", xl: "16px" }}>
+                <SimpleGrid columns={{ base: 1, md: 1, xl: 1 }} gap="20px" mb="20px">
+                    {tableData && <VendorsTableComponent tableData={tableData} />}
+                </SimpleGrid>
+            </Box>
+        </>
+    );
+};
+
+export default VendorsComponent;
---
+++ src/app/admin/modules/purchases-module/vendors/VendorsTableComponent.tsx
@@ -0,0 +1,187 @@
+import { Flex, Box, Table, Checkbox, Tbody, Td, Text, Th, Thead, Tr, useColorModeValue } from "@chakra-ui/react";
+import * as React from "react";
+
+import { createColumnHelper, flexRender, getCoreRowModel, getSortedRowModel, SortingState, useReactTable } from "@tanstack/react-table";
+
+import Card from "components/card/Card";
+
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
+
+type RowObj = {
+    primaryContactFirstName: [string, boolean];
+    primaryContactLastName: [string, boolean];
+    vendorPhone: number;
+    vendorDisplayName: string;
+    companyName: string;
+};
+
+const columnHelper = createColumnHelper<RowObj>();
+
+function VendorsTableComponent(props: { tableData: any }) {
+    const { tableData } = props;
+    const [sorting, setSorting] = React.useState<SortingState>([]);
+    const textColor = useColorModeValue("secondaryGray.900", "white");
+    const borderColor = useColorModeValue("gray.200", "whiteAlpha.100");
+    let defaultData = tableData;
+
+    const columns = [
+        columnHelper.accessor("primaryContactFirstName", {
+            id: "primaryContactFirstName",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    FIRST NAME
+                </Text>
+            ),
+            cell: (info: any) => (
+                <Flex align="center">
+                    <Checkbox defaultChecked={info.getValue()[1]} colorScheme="brandScheme" me="10px" />
+                    <Text color={textColor} fontSize="sm" fontWeight="700">
+                        <ChakraLink as={ReactRouterLink} to={`/admin/modules/purchases/vendor/${info.row.original.id}`}>
+                            {info.getValue()}
+                        </ChakraLink>
+                    </Text>
+                </Flex>
+            ),
+        }),
+
+        columnHelper.accessor("primaryContactLastName", {
+            id: "primaryContactLastName",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    LAST NAME
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("vendorDisplayName", {
+            id: "vendorDisplayName",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    DISPLAY NAME
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("vendorPhone", {
+            id: "vendorPhone",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    PHONE NUMBER
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("companyName", {
+            id: "companyName",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    COMPANY'S NAME
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+    ];
+
+    const [data, setData] = React.useState(() => [...defaultData]);
+
+    const table = useReactTable({
+        data,
+        columns,
+        state: {
+            sorting,
+        },
+        onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        debugTable: true,
+    });
+
+    return (
+        <Card flexDirection="column" w="100%" px="0px" overflowX={{ sm: "scroll", lg: "hidden" }}>
+            <Box>
+                <Table variant="simple" color="gray.500" mb="24px" mt="12px">
+                    <Thead>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <Tr key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <Th
+                                            key={header.id}
+                                            colSpan={header.colSpan}
+                                            pe="10px"
+                                            borderColor={borderColor}
+                                            cursor="pointer"
+                                            onClick={header.column.getToggleSortingHandler()}
+                                        >
+                                            <Flex
+                                                justifyContent="space-between"
+                                                align="center"
+                                                fontSize={{
+                                                    sm: "10px",
+                                                    lg: "12px",
+                                                }}
+                                                color="gray.400"
+                                            >
+                                                {flexRender(header.column.columnDef.header, header.getContext())}
+                                                {{
+                                                    asc: "",
+                                                    desc: "",
+                                                }[header.column.getIsSorted() as string] ?? null}
+                                            </Flex>
+                                        </Th>
+                                    );
+                                })}
+                            </Tr>
+                        ))}
+                    </Thead>
+                    <Tbody>
+                        {table.getRowModel().rows.map((row) => {
+                            return (
+                                <Tr key={row.id}>
+                                    {row.getVisibleCells().map((cell) => {
+                                        return (
+                                            <Td
+                                                key={cell.id}
+                                                fontSize={{ sm: "14px" }}
+                                                minW={{
+                                                    sm: "150px",
+                                                    md: "200px",
+                                                    lg: "auto",
+                                                }}
+                                                borderColor="transparent"
+                                            >
+                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                            </Td>
+                                        );
+                                    })}
+                                </Tr>
+                            );
+                        })}
+                    </Tbody>
+                </Table>
+            </Box>
+        </Card>
+    );
+}
+
+export default VendorsTableComponent;
