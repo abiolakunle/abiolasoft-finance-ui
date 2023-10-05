d1d8c8fd7a08efba0bd327dd9b086f8136fd1bdc
Olufemi Olaosebikan <olufemiolaosebikan.t@gmail.com>
2023-10-05 04:58:12 -0700

    Update PurchaseModuleLayout.tsx

--- src/app/admin/modules/purchases-module/PurchaseModuleLayout.tsx
+++ src/app/admin/modules/purchases-module/PurchaseModuleLayout.tsx
@@ -9,20 +9,12 @@ import PurchaseOrderFormComponent from "./purchase-order-form/PurchaseOrderFormC
 import PurchaseOrderComponent from "./purchase-order/PurchaseOrderComponent";
 import VendorComponent from "./vendor/VendorComponent";
 import VendorFormComponent from "./vendor-form/VendorFormComponent";
+import { getUserInfo } from "utils/auth";
 
-const navRoutes = [
-    {
-        name: "Dashboard",
-        path: "/",
-        icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
-        component: <PurchasesDashboardComponent />,
-    },
-    {
-        name: "Purchase Orders",
-        path: "/purchase-orders",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <PurchaseOrdersComponent />,
-    },
+
+
+
+    ,
     // {
     //     name: "Purchase Receives",
     //     path: "/purchase-receives",
@@ -41,61 +33,91 @@ const navRoutes = [
     //     icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
     //     component: <PurchaseReceiptsComponent />,
     // },
-    {
-        name: "Vendors",
-        path: "/vendors",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <VendorsComponent />,
-    },
-];
+    
+// ];
 
-const purchaseRoutes = [
-    ...navRoutes,
-    {
-        name: "New Purchase Order",
-        path: "/purchase-orders/new",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <PurchaseOrderFormComponent />,
-        excludeFromSideNav: true,
-    },
-    {
-        name: "New Purchase Order",
-        path: "/purchase-orders/:id/edit",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <PurchaseOrderFormComponent />,
-        excludeFromSideNav: true,
-    },
-    {
-        name: "Purchase Order",
-        path: "/purchase-orders/:id",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <PurchaseOrderComponent />,
-        excludeFromSideNav: true,
-    },
-    {
-        name: "Vendor",
-        path: "/vendors/:id",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <VendorComponent />,
-        excludeFromSideNav: true,
-    },
-    {
-        name: "New Vendor",
-        path: "/vendors/new",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <VendorFormComponent />,
-        excludeFromSideNav: true,
-    },
-    {
-        name: "Edit Vendor",
-        path: "/vendors/:id/edit",
-        icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
-        component: <VendorFormComponent />,
-        excludeFromSideNav: true,
-    },
-];
+
 
 const PurchaseModuleLayout = () => {
+
+    const navRoutes = [
+        {
+            name: "Dashboard",
+            path: "/",
+            icon: <Icon as={MdHome} width="20px" height="20px" color="inherit" />,
+            component: <PurchasesDashboardComponent />,
+        },
+    
+    ]
+    
+    const user = getUserInfo()
+
+    if (user?.permissions?.includes("View Purchase Orders")) {
+        navRoutes.push({
+            name: "Purchase Orders",
+            path: "/purchase-orders",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <PurchaseOrdersComponent />,
+        })
+    }
+
+    if (user?.permissions?.includes("View Vendors")) {
+        navRoutes.push({
+            name: "Vendors",
+            path: "/vendors",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <VendorsComponent />,
+        })
+    }
+
+    const purchaseRoutes = [
+        ...navRoutes,
+        {
+            name: "New Purchase Order",
+            path: "/purchase-orders/new",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <PurchaseOrderFormComponent />,
+            excludeFromSideNav: true,
+        },
+        {
+            name: "New Purchase Order",
+            path: "/purchase-orders/:id/edit",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <PurchaseOrderFormComponent />,
+            excludeFromSideNav: true,
+        },
+        {
+            name: "Purchase Order",
+            path: "/purchase-orders/:id",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <PurchaseOrderComponent />,
+            excludeFromSideNav: true,
+        },
+        {
+            name: "Vendor",
+            path: "/vendors/:id",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <VendorComponent />,
+            excludeFromSideNav: true,
+        },
+        {
+            name: "New Vendor",
+            path: "/vendors/new",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <VendorFormComponent />,
+            excludeFromSideNav: true,
+        },
+        {
+            name: "Edit Vendor",
+            path: "/vendors/:id/edit",
+            icon: <Icon as={MdList} width="20px" height="20px" color="inherit" />,
+            component: <VendorFormComponent />,
+            excludeFromSideNav: true,
+        },
+    ];
+
+
+
     return (
         <NavigationComponent baseRoute="/admin/modules/purchases" routes={purchaseRoutes}>
             <Routes>
