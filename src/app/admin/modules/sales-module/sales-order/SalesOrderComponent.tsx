233701f5d86e1658926caeb66878f10a504d7b13
Olufemi Olaosebikan <olufemiolaosebikan.t@gmail.com>
2023-10-09 04:23:05 -0700

    sale order and sales order form

--- src/app/admin/modules/sales-module/sales-order/SalesOrderComponent.tsx
+++ src/app/admin/modules/sales-module/sales-order/SalesOrderComponent.tsx
@@ -1,21 +1,25 @@
-import { CloseButton, Button, Flex, Heading, IconButton, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import { CloseButton, Button, Flex, Heading, IconButton, Menu, MenuButton, MenuList, MenuItem, useDisclosure, useToast } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
-
 import { MdEdit } from "react-icons/md";
 import SalesOrderFormComponent from "../sales-order-form/SalesOrderFormComponent";
 import { Link as ReactRouterLink, useNavigate, useParams } from "react-router-dom";
 import { Link as ChakraLink } from "@chakra-ui/react";
-import { apiBaseUrl } from "environment";
-import axios from "axios";
+import axiosRequest from "utils/api";
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody } from "@chakra-ui/react";
+import IfUserIsPermitted from "app-components/if-user-is-permitted/IfUserIsPermitted";
 
 const SalesOrderComponent = () => {
     const { id } = useParams();
 
     const navigate = useNavigate();
 
+    const toast = useToast();
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     const instantInvoice = async () => {
         try {
-            const response = await axios.post(apiBaseUrl + "Sales/ConvertSalesOrderToInvoice", { orderId: id });
+            const response = await axiosRequest.post("Sales/ConvertSalesOrderToInvoice", { orderId: id });
 
             if (response.status === 200) {
                 navigate(`/admin/modules/sales/sales-orders/${id}`);
@@ -31,6 +35,23 @@ const SalesOrderComponent = () => {
         navigate("/admin/modules/sales/invoices/new", { state: { saleOrderId: id } });
     };
 
+    const submit = async () => {
+        try {
+            await axiosRequest.delete(`Sales/DeleteSalesOrder`, { data: { id } });
+            toast({
+                title: "Success",
+                description: "Deleted Successfully",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom-right",
+            });
+            navigate(`/admin/modules/sales/sales-orders`);
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
     return (
         <>
             <Flex
@@ -44,26 +65,68 @@ const SalesOrderComponent = () => {
                     xl: "space-between",
                 }}
                 gap="20px"
+                flexWrap={{ sm: "wrap", md: "nowrap" }}
             >
                 <Heading as="h4" size="md">
                     Sales Order
                 </Heading>
 
-                <Flex h="fit-content" alignItems="center" justifyContent="space-between" gap="20px">
+                <Flex
+                    h="fit-content"
+                    width={{ sm: "100%", md: "fit-content" }}
+                    flexWrap={{ sm: "wrap", md: "nowrap" }}
+                    alignItems="center"
+                    justifyContent="space-between"
+                    gap="20px"
+                >
+                    <IfUserIsPermitted to="Edit Sales Order">
+                        <ChakraLink order={{ sm: "1" }} as={ReactRouterLink} to={`/admin/modules/sales/sales-orders/${id}/edit`}>
+                            <IconButton variant="outline" colorScheme="brand" borderRadius="10px" aria-label="Edit" fontSize="20px" icon={<MdEdit />} />
+                        </ChakraLink>
+                    </IfUserIsPermitted>
+
                     <Menu>
-                        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                            Convert
+                        <IfUserIsPermitted to="Convert Sales Order To Invoice">
+                            <MenuButton order={{ sm: "3", md: "2" }} width="100%" minW="120px" as={Button} rightIcon={<ChevronDownIcon />}>
+                                Convert
+                            </MenuButton>
+
+                            <MenuList>
+                                <MenuItem onClick={convertToInvoice}>Convert to Invoice</MenuItem>
+                                <MenuItem onClick={instantInvoice}>Instant Invoice</MenuItem>
+                            </MenuList>
+                        </IfUserIsPermitted>
+                    </Menu>
+
+                    <Menu>
+                        <MenuButton order={{ sm: "4", md: "3" }} width="100%" as={Button} rightIcon={<ChevronDownIcon />}>
+                            More
                         </MenuButton>
                         <MenuList>
-                            <MenuItem onClick={convertToInvoice}>Convert to Invoice</MenuItem>
-                            <MenuItem onClick={instantInvoice}>Instant Invoice</MenuItem>
+                            <IfUserIsPermitted to="Delete Sales Order">
+                                <MenuItem onClick={onOpen}>Delete</MenuItem>
+                            </IfUserIsPermitted>
                         </MenuList>
-                    </Menu>
-                    <ChakraLink as={ReactRouterLink} to={`/admin/modules/sales/sales-orders/${id}/edit`}>
-                        <IconButton variant="outline" colorScheme="brand" borderRadius="10px" aria-label="Call Fred" fontSize="20px" icon={<MdEdit />} />
-                    </ChakraLink>
 
-                    <ChakraLink as={ReactRouterLink} to={`/admin/modules/sales/sales-orders`}>
+                        <Modal isOpen={isOpen} onClose={onClose}>
+                            <ModalOverlay />
+                            <ModalContent>
+                                <ModalHeader>Delete Sales Order</ModalHeader>
+
+                                <ModalBody>Are You Sure You Want To Delete?</ModalBody>
+                                <ModalFooter>
+                                    <Button variant="ghost" onClick={onClose}>
+                                        Cancel
+                                    </Button>
+                                    <Button colorScheme="red" onClick={submit} ml={3}>
+                                        Delete
+                                    </Button>
+                                </ModalFooter>
+                            </ModalContent>
+                        </Modal>
+                    </Menu>
+
+                    <ChakraLink order={{ sm: "2", md: "4" }} as={ReactRouterLink} to={`/admin/modules/sales/sales-orders`}>
                         <CloseButton size="lg" />
                     </ChakraLink>
                 </Flex>
