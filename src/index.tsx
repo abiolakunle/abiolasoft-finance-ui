db01712693baa5b90f6874bca5c5e22002d41166
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2023-10-28 01:52:51 +0100

    tenant path update

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/css/App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
@@ -10,6 +10,8 @@ import { Provider, useDispatch, useSelector } from "react-redux";
 import axiosRequest from "utils/api";
 import { hideProgress } from "state/slices/progressSlice";
 import { store } from "state/store";
+import axios from "axios";
+import { apiBaseUrl } from "environment";
 
 let responseInterceptorActive = false;
 
@@ -22,7 +24,7 @@ const App = () => {
     });
 
     axiosRequest.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
-    axiosRequest.defaults.headers.common["TenantKey"] = window.location.hostname.split(".")[0];
+    const [tenantKey, setTenantKey] = useState("");
 
     if (!responseInterceptorActive) {
         axiosRequest.interceptors.response.use(
@@ -49,17 +51,63 @@ const App = () => {
         responseInterceptorActive = true;
     }
 
+    useEffect(() => {
+        getAccountByIdentifier(tenantKey, "sub-domain");
+    }, [responseInterceptorActive]);
+
+    const getAccountByIdentifier = async (id: string, option: "sub-domain" | "first-path" | "local-storage") => {
+        await axios
+            .get(`${apiBaseUrl}UserManagement/GetAccountByIdentifier?accountIdentifier=${id}`)
+            .then(() => {
+                if (option === "first-path") {
+                    localStorage.setItem("tenantKey", id);
+                }
+
+                axiosRequest.defaults.headers.common["TenantKey"] = id;
+                setTenantKey(id);
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+
+                if (option === "local-storage") {
+                    toast({
+                        title: "Error",
+                        description: error.response.data.message,
+                        status: "error",
+                        duration: 5000,
+                        isClosable: true,
+                        position: "bottom-right",
+                    });
+                }
+
+                if (option === "sub-domain") {
+                    getAccountByIdentifier(window.location.pathname.split("/")[1], "first-path");
+                }
+
+                if (option === "first-path") {
+                    getAccountByIdentifier(localStorage.getItem("tenantKey"), "local-storage");
+                }
+            });
+    };
+
     return (
         <>
             {showProgress && <Progress size="sm" isIndeterminate width="full" position="fixed" top="0" left="0" zIndex="10" />}
-            <Router>
-                <Routes>
-                    <Route path="" element={<AuthLayout />} />
-                    <Route path="auth/sign-in" element={<AuthLayout />} />
-                    <Route path="admin/*" element={<AdminLayout />} />
-                    {/* <Redirect from="/" to="/admin" /> */}
-                </Routes>
-            </Router>
+            {tenantKey && (
+                <Router>
+                    <Routes>
+                        <Route path="" element={<AuthLayout />} />
+                        <Route path={tenantKey} element={<AuthLayout />} />
+
+                        <Route path="auth/sign-in" element={<AuthLayout />} />
+                        <Route path={`${tenantKey}/auth/sign-in`} element={<AuthLayout />} />
+
+                        <Route path="admin/*" element={<AdminLayout />} />
+                        <Route path={`${tenantKey}/admin/*`} element={<AdminLayout />} />
+                        {/* <Redirect from="/" to="/admin" /> */}
+                    </Routes>
+                </Router>
+            )}
         </>
     );
 };
