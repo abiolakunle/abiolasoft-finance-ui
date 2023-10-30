eae028abfcf5665f13712c71a6a2ea269fb872c4
David smart <112566573+davidgraphix@users.noreply.github.com>
2023-10-30 14:07:26 +0100

    Done

---
+++ src/app/admin/modules/purchases-module/Bills/BillsTableComponent.tsx
@@ -0,0 +1,164 @@
+import { Flex, Checkbox, Text, useColorModeValue } from "@chakra-ui/react";
+import Card from "components/card/Card";
+import * as React from "react";
+import { createColumnHelper } from "@tanstack/react-table";
+import { Link as ReactRouterLink, useParams } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { formatDateTime } from "utils/dateUtils";
+import { useEffect } from "react";
+import GeneralTable from "app-components/general-table/GeneralTable";
+
+type RowObj = {
+    date: string;
+    bill: string;
+    vendorName: string;
+    referenceNumber: string;
+    status: number;
+    dueDate: string;
+    amount: string;
+    balanceDue: string;
+};
+
+const columnHelper = createColumnHelper<RowObj>();
+
+function BillsTableComponent(props: { tableData: any }) {
+    const { tableData } = props;
+    const textColor = useColorModeValue("secondaryGray.900", "white");
+
+    const { organizationId } = useParams();
+
+    const columns = [
+        columnHelper.accessor("date", {
+            id: "date",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    DATE
+                </Text>
+            ),
+            cell: (info: any) => (
+                <Flex align="center">
+                    <Checkbox defaultChecked={info.getValue()[1]} colorScheme="brandScheme" me="10px" />
+                    <Text color={textColor} fontSize="sm" fontWeight="700">
+                        <ChakraLink
+                            as={ReactRouterLink}
+                            to={`/admin/organizations/${organizationId}/modules/purchases/purchase-orders/${info.row.original.id}`}
+                        >
+                            {formatDateTime(info.getValue())}
+                        </ChakraLink>
+                    </Text>
+                </Flex>
+            ),
+        }),
+        columnHelper.accessor("bill", {
+            id: "bill",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    BILL#
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("referenceNumber", {
+            id: "referenceNumber",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    REFERENCE NUMBER#
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("vendorName", {
+            id: "vendorName",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    VENDOR NAME
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("status", {
+            id: "status",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    STATUS
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("dueDate", {
+            id: "dueDate",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    DUE DATE
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("amount", {
+            id: "amount",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    AMOUNT
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+
+        columnHelper.accessor("balanceDue", {
+            id: "balanceDue",
+            header: () => (
+                <Text justifyContent="space-between" align="center" fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+                    BALANCE DUE
+                </Text>
+            ),
+            cell: (info) => (
+                <Text color={textColor} fontSize="sm" fontWeight="700">
+                    {info.getValue()}
+                </Text>
+            ),
+        }),
+    ];
+
+    const [data, setData] = React.useState(() => [...tableData]);
+
+    useEffect(() => {
+        setData(tableData);
+    }, [tableData]);
+
+    return (
+        <Card flexDirection="column" w="100%" px="0px" overflowX={{ sm: "scroll", lg: "hidden" }}>
+            <GeneralTable data={data} columns={columns} />
+        </Card>
+    );
+}
+
+export default BillsTableComponent;
