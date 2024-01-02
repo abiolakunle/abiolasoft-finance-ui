9b43cd970664dae8f1730faebac50b85853946c0
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2024-01-02 22:03:17 +0100

    Update ReceiptComponent.tsx

--- src/app/admin/modules/sales-module/receipt/ReceiptComponent.tsx
+++ src/app/admin/modules/sales-module/receipt/ReceiptComponent.tsx
@@ -1,9 +1,8 @@
 import { CloseButton, Button, Flex, Heading, IconButton, Menu, MenuButton, MenuList, MenuItem, useDisclosure, useToast } from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { MdEdit } from "react-icons/md";
-import { Link as ReactRouterLink, useNavigate, useParams } from "react-router-dom";
+import { Link as ReactRouterLink, useParams } from "react-router-dom";
 import { Link as ChakraLink } from "@chakra-ui/react";
-import axiosRequest from "utils/api";
 import IfUserIsPermitted from "app-components/if-user-is-permitted/IfUserIsPermitted";
 import DeleteModal from "app-components/delete-modal/DeleteModal";
 import { ReceiptFormComponent } from "../receipt-form/ReceiptFormComponent";
@@ -11,50 +10,11 @@ import { ReceiptFormComponent } from "../receipt-form/ReceiptFormComponent";
 export const ReceiptComponent = () => {
     const { id, organizationId } = useParams();
 
-    const navigate = useNavigate();
-
-    const toast = useToast();
-
     const redirect = `/admin/organizations/${organizationId}/modules/sales/receipts/`;
 
     const deleteEndpoint = `Sales/DeleteReceipt`;
 
     const { isOpen, onOpen, onClose } = useDisclosure();
-
-    const instantInvoice = async () => {
-        try {
-            const response = await axiosRequest.post("Sales/ConvertReceiptToInvoice", { orderId: id });
-
-            if (response.status == 200) {
-                navigate(`/admin/organizations/${organizationId}/modules/sales/receipts/${id}`);
-            } else {
-                console.error("Error creating item");
-            }
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    };
-
-    const convertToInvoice = () => {
-        navigate(`/admin/organizations/${organizationId}/modules/sales/invoices/new`, { state: { receiptId: id } });
-    };
-
-    const submit = async () => {
-        try {
-            await axiosRequest.delete(`Sales/DeleteReceipt`, { data: { id } });
-            toast({
-                title: "Success",
-                description: "Deleted Successfully",
-                status: "success",
-                duration: 5000,
-                isClosable: true,
-                position: "bottom-right",
-            });
-            navigate(`/admin/organizations/${organizationId}/modules/sales/receipts`);
-        } catch (error) {
-            console.error("Error:", error);
-        }
-    };
 
     return (
         <>
@@ -88,19 +48,6 @@ export const ReceiptComponent = () => {
                             <IconButton variant="outline" colorScheme="brand" borderRadius="10px" aria-label="Edit" fontSize="20px" icon={<MdEdit />} />
                         </ChakraLink>
                     </IfUserIsPermitted>
-
-                    <Menu>
-                        <IfUserIsPermitted to="Convert Receipt To Invoice">
-                            <MenuButton order={{ sm: "4", md: "2" }} width="100%" minW="120px" as={Button} rightIcon={<ChevronDownIcon />}>
-                                Convert
-                            </MenuButton>
-
-                            <MenuList>
-                                <MenuItem onClick={convertToInvoice}>Convert to Invoice</MenuItem>
-                                <MenuItem onClick={instantInvoice}>Instant Invoice</MenuItem>
-                            </MenuList>
-                        </IfUserIsPermitted>
-                    </Menu>
 
                     <Menu>
                         <MenuButton order={{ sm: "2", md: "3" }} width={{ sm: "wrap", xl: "100%" }} as={Button} rightIcon={<ChevronDownIcon />}>
